534be056f020960f9ea890854f89d29db4a34889
Davis Haupt <davis.haupt@gmail.com>
2018-10-15 15:17:03 -0400

    functioning submission route

--- pca/urls.py
+++ pca/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('submitted', views.register, name='register')
 ]
--- pca/views.py
+++ pca/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.http import HttpResponse, JsonResponse, Http404
+from django.template import loader
+from .models import *
 
 
 def index(request):
-    return HttpResponse("Hello, world! This should be PCA eventually.")
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({}, request))
+
+
+def register(request):
+    if request.method == 'POST':
+        course_code = request.POST.get('course', None)
+        email_address = request.POST.get('email', None)
+        carrier = request.POST.get('carrier', None)
+        phone = request.POST.get('phone', None)
+
+        course, section = get_course_and_section(course_code, get_current_semester())
+        email = Email.objects.get_or_create(address=email_address)[0]
+        registration = Registration(section=section)
+        registration.save()
+        registration.emails.add(email)
+
+        return JsonResponse({
+            'course': course.__str__(),
+            'section': section.__str__(),
+            'registration': registration.__str__()
+        })
+    else:
+        raise Http404('GET not accepted')
