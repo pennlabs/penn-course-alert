61f155dd50eab21cd6e270d300fff053efb4d8a6
Davis Haupt <davis.haupt@gmail.com>
2018-11-02 17:47:19 -0400

    fixed static files

---
+++ PennCourseAlert/deploy_settings.py
@@ -0,0 +1,27 @@
+# Settings when deployed to Dokku
+from .settings import *  # noqa
+
+# Disable Django's own staticfiles handling in favour of WhiteNoise, for
+# greater consistency between gunicorn and `./manage.py runserver`. See:
+# http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
+INSTALLED_APPS.remove('django.contrib.staticfiles')
+INSTALLED_APPS.extend([
+    'whitenoise.runserver_nostatic',
+    'django.contrib.staticfiles',
+])
+
+MIDDLEWARE.remove('django.middleware.security.SecurityMiddleware')
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+] + MIDDLEWARE
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers (feel free to make this more specific)
+ALLOWED_HOSTS = ['penncoursealert.com', 'www.penncoursealert.com']
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- PennCourseAlert/wsgi.py
+++ PennCourseAlert/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PennCourseAlert.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PennCourseAlert.deploy_settings')
 
 application = get_wsgi_application()
