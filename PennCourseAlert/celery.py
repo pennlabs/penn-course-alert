9e209cfe1d68b53d898e82cc62d9ce5c787bfbb0
Davis Haupt <davis.haupt@gmail.com>
2019-08-05 22:36:11 -0400

    default to dev settings

--- PennCourseAlert/celery.py
+++ PennCourseAlert/celery.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery
 from django.conf import settings
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PennCourseAlert.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PennCourseAlert.settings.dev')
 
 app = Celery('PennCourseAlert', broker=settings.MESSAGE_BROKER_URL)
 
